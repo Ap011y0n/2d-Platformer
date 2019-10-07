472c4fc65ac8ca2d8ed91969df4b04357a30c9c2
JBGelsenorJ <javi_belda@live.com>
2019-10-07 20:32:29 +0200

    pruebas

--- Dev05_Handout/Motor2D/j1Map.cpp
+++ Dev05_Handout/Motor2D/j1Map.cpp
@@ -110,7 +110,7 @@ bool j1Map::Load(const char* file_name)
 {
 	bool ret = true;
 	p2SString tmp("%s%s", folder.GetString(), file_name);
-	image = App->tex->Load("maps/tmw_desert_spacing.png");
+	image = App->tex->Load("maps/warped.png");
 	pugi::xml_parse_result result = map_file.load_file(tmp.GetString());
 
 	if (result == NULL)
